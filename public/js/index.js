9292cda8755c323bef9011342242371ecefb9e39
KobayashiTao <karolsinsel@gmail.com>
2025-03-11 01:59:08 -0600

    Todo

---
+++ public/js/index.js
@@ -0,0 +1,45 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+    x.className += " responsive";
+    } else {
+    x.className = "topnav";
+    }
+}
+function scrollMain() {
+var x = document.getElementById("myTopnav");
+x.className = "topnav";
+}
+function scrollPopular(){
+document.getElementById('scrollButton').addEventListener('click', function() {
+    document.getElementById('popular-section').scrollIntoView({ behavior: 'smooth' });
+});
+}
+document.addEventListener("DOMContentLoaded", function () {
+const track = document.querySelector(".carousel-track");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+let index = 0;
+
+function updateCarousel() {
+    const itemWidth = document.querySelector(".carousel-item").offsetWidth;
+    track.style.transform = `translateX(${-index * itemWidth}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+    if (index < 4) {
+        index++;
+        updateCarousel();
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    if (index > 0) {
+        index--;
+        updateCarousel();
+    }
+});
+
+window.addEventListener("resize", updateCarousel);
+});
