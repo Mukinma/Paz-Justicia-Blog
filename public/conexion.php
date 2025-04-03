b9a79205981e16445af2c6ea931302b3b57177d1
KobayashiTao <karolsinsel@gmail.com>
2025-04-03 09:38:05 -0600

    nada

--- public/conexion.php
+++ public/conexion.php
@@ -1,3 +1,12 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "bdadmin";
+
+$conexion = new mysqli($server, $user, $pass, $db);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,22 +20,40 @@
         <img src="./images/PeaceInProgress.jpeg" alt="Imagen de teclado con la palabra peace" class="login-image">
         <div class="right-container">
             <h2>WELCOME!</h2>
-            <form action="login.php" method="POST" class="login-form">
+            <form action="conexion.php" method="POST" class="login-form">
                 <div class="login-container">
                     <h3>Log In</h3>
                     <div class="form-group">
-                        <input type="email" id="email" name="email" placeholder="Email" required = "true">
+                        <input type="email" id="email" name="email" placeholder="Email" required>
                     </div>
                     <div class="form-group">
-                        <input type="password" id="password" name="password" placeholder="Password" minlength="8" pattern=".{6,}" required = "true">
+                        <input type="password" id="password" name="password" placeholder="Password" required>
                     </div>
                     <a href="#" class="forgot-password"><label for="recordar">Forgot Password?</label></a>
                     <div class="button-group">
-                        <button type="submit" class="login-button">Sign up</button>
+                        <button type="submit" class="login-button" name="registro">Sign up</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </body>
+
+<?php
+if(isset($_POST['registro'])){
+    $email = $_POST ['email'];
+    $password = $_POST ['password'];
+
+    $insertardatos = "INSERT INTO dbad VALUES('$email','$password','')";
+    $ejecutarinsertar = mysqli_query ($conexion, $insertardatos);
+
+    if($ejecutarinsertar){
+        header("Location: indexM.html");
+        exit();
+    } else{
+        echo "error al insertar los datos";
+    }
+}
+?>
+
 </html>
